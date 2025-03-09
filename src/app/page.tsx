5145502b882762b408582d8c769e0465dc59b1bb
Srinath vunnava <vusrinath@gmail.com>
2025-03-09 18:00:56 -0500

    Uncommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,22 @@ import ProgressBar from '../components/ProgressBar/progressBar';
 import FloderExp from '../components/FolderExplorer/FolderExp';
 
 
-export default function Home() {
-
+let Progress = () => {
   let list = [60, 80];
-
-  let prg = list.map(it => {
+  return list.map(it => {
     return (
       <div style={{ marginTop: 30 }} >
         <ProgressBar key={it} pg={it} />
       </div>
     )
   })
+}
+
+export default function Home() {
 
   return (
     <div>
+      <Progress />
       <FloderExp />
     </div>
   )
